f4fea53b7d0ddc057926156b4da69bb5c0e8ba0b
Manuel Montoro <erlukasgrijander@gmail.com>
2023-12-19 14:44:36 +0100

    Práctica obligatoria 4.06: WIP

---
+++ Unidad 04/actividad406/libreria/src/prog/unidad04/practica406/libreria/Fecha.java
@@ -0,0 +1,14 @@
+package prog.unidad04.practica406.libreria;
+
+public class Fecha implements ConvertibleATexto{
+
+  public Fecha(int i, int j, int k) {
+    // TODO Auto-generated constructor stub
+  }
+
+  @Override
+  public String aTexto() {
+    return "";
+  }
+
+}
