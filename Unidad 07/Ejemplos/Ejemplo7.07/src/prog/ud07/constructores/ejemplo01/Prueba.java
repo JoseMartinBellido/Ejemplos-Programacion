c511cfaf11efbc0010ea46e132d7c215175d33b2
Manuel Montoro <erlukasgrijander@gmail.com>
2024-02-06 12:18:25 +0100

    Unidad 07: Ejemplo 7.07

---
+++ Unidad 07/Ejemplos/Ejemplo7.07/src/prog/ud07/constructores/ejemplo01/Prueba.java
@@ -0,0 +1,10 @@
+package prog.ud07.constructores.ejemplo01;
+
+public class Prueba {
+
+  public static void main(String[] args) {
+    // No se puede hacer porque SubClase no tiene constructor con cadena y no se hereda
+    //SubClase sub = new SubClase("Pepe");
+  }
+
+}
---
+++ Unidad 07/Ejemplos/Ejemplo7.07/src/prog/ud07/constructores/ejemplo02/Prueba.java
@@ -0,0 +1,10 @@
+package prog.ud07.constructores.ejemplo02;
+
+public class Prueba {
+
+  public static void main(String[] args) {
+    SubClase sub1 = new SubClase();
+    SubClase2 sub2 = new SubClase2();
+  }
+
+}
